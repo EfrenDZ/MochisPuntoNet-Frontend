79eb19bcc75a4fcfd915e2d6dd61dc19f3fed240
EfrenDZ <j.efrendz2004@gmail.com>
2026-02-25 09:20:49 -0700

    cambios

---
+++ index.js
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/assets/mochis_punto_net_icon-DTnvgSeV.png" />
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    
+    <link rel="manifest" href="/manifest.json" />
+    
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="MochisTV">
+
+    <title>Mochis Punto Net
+    </title>
+
+    <style>
+      body, html {
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden; /* Oculta barras de scroll */
+        user-select: none; /* Evita que seleccionen texto */
+      }
+      #root {
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+    <script type="module" crossorigin src="/assets/index-Cxhwvl9P.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-Jl8pUSRj.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
